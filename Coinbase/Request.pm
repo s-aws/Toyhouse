c7fced7c644f4215e54d58b99ba3ad489a8cea6e
S Groom <the.count@s-aws.com>
2021-01-17 17:30:48 -0500

    improved throttle handling

--- Coinbase/Request.pm
+++ Coinbase/Request.pm
@@ -1,7 +1,9 @@
 package Toyhouse::Coinbase::Request;
 use Readonly;
+use Data::Dumper;
 use Time::HiRes qw/sleep/;
 use Toyhouse::UserAgent;
+use Mojo::Promise;
 use Mojo::Base qw/-strict -signatures/;
 use Class::Struct ('Toyhouse::Coinbase::Request' => {
 	api_path => '$',
@@ -24,6 +26,7 @@ sub build($self) { # required for signed requests
 sub send($self) {
 	die 'method and api_path are required' unless $self->method() && $self->api_path();
 	my ($r,$o);
+	my $sleep_time = 1;
 	my $method = lc $self->method();
 	my $url = join('/', $URL, $self->api_path());
 	my $body = [do {$self->body()? (json => $self->body()) : ()}];
@@ -33,7 +36,7 @@ sub send($self) {
  	my $page_counter = 0; my $last_token;
  	do {
  		my $pagination = ();
-
+		$o->res->error(undef) if $o && $o->res->error();
  		if ($o && (my $token = $o->res->content->headers->header('cb-after'))) {
  			$pagination = ($query_string ? '&' : '?') . 'after='. $token. '&limit=100';
  			$last_token = $token
@@ -46,11 +49,11 @@ sub send($self) {
  		$o = $ua->$method( $full_url => @$body );
 
  		if ($o->res->content->headers->header('content-length') > 2) {
- 			if ($r) {
-	 			if ($o->res->content->asset->{content} =~ /exceeded/) { #throttled
-	 				print STDERR $o->res->content->asset->{content}. " sleeping 1 second\n";
-		 			sleep 1;
-		 			$o->res->content->headers->header('cb-after', $last_token);
+ 			if ($r || $o->res->error()) {
+	 			if ($o->res->error() && ($o->res->error->{code} == 429)) { #throttled
+	 				print STDERR $o->res->content->asset->{content}. " sleeping $sleep_time second\n";
+		 			sleep $sleep_time; $sleep_time *= 2;
+		 			$o->res->content->headers->header('cb-after', $last_token) if $last_token;
 		 			$page_counter--;
 	 			}
 	 			else {
@@ -58,7 +61,7 @@ sub send($self) {
 	 			}
  			}
  			else {
- 				$r = $o;
+				$r = $o if $o && (!$o->res->error() || ($o->res->error->{code} != 429));
  			}
  		}
  		else {
@@ -66,10 +69,13 @@ sub send($self) {
  		}
  		$page_counter++;
  	}
- 		while $page_counter <= ($self->max_pages() || 500) && $o->res->content->headers->header('cb-after'); 
+ 		while (($page_counter <= ($self->max_pages() || 500) && $o->res->content->headers->header('cb-after')) || ($o->res->error() && ($o->res->error->{code} == 429)) ); 
 
-	unless ( $r->res->error() ) { $r->res->content() }
+	$r->res->error(undef) if $r->res->error() && ($r->res->error->{code} == 429); #reset error throttle flag if it exists
+
+	unless ( $r->res->error() && ($r->res->error->{code} != 404)) { $r->res->content() }
 	else { die "Did not complete as expected. ", $r->res->error->{message} } 	
 }
+
 
 1
